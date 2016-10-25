5cdfafacf3407952965b76fd0f35336b23ae9cef
daslicht <daslicht@ansolas.de>
2016-10-25 15:23:45 +0200

    Added Simple Firebase Item retrieval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import {AngularFire, FirebaseListObservable,FirebaseObjectObservable} from 'angularfire2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  
   title = 'app works!';
+  
+  items: FirebaseListObservable<any[]>;
+
+  item: FirebaseObjectObservable<any>;
+
+  constructor(af: AngularFire) {
+    this.items = af.database.list('/items');
+    this.item = af.database.object('/item');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAzrtKXwKL5kAoa9dbGt9x32YWA7Yi_Jec',
+  authDomain: 'test-fad8a.firebaseapp.com',
+  databaseURL: 'https://test-fad8a.firebaseio.com',
+  storageBucket: 'test-fad8a.appspot.com',
+  messagingSenderId: "113730117393"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
