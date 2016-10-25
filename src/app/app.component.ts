0e319ff22ad745a10cdd27f76c1921dec95773ff
daslicht <daslicht@ansolas.de>
2016-10-25 18:32:14 +0200

    Added Firebase OAuth Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,49 @@
 import { Component } from '@angular/core';
-import {AngularFire,FirebaseObjectObservable} from 'angularfire2';
+import {
+	AngularFire,
+	FirebaseObjectObservable,
+	AuthProviders
+} from 'angularfire2';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
-  
-  title = 'app works!';
 
-  item: FirebaseObjectObservable<any>;
+	title = 'app works!';
+	
+	user = null;
+	item: FirebaseObjectObservable<any>;
 
-  constructor(af: AngularFire) {
-    this.item = af.database.object('/item');
-  }
+	constructor(private af: AngularFire) {
+
+		this.af.auth.subscribe(user => {
+			if (user) {
+				// user logged in
+				this.user = user;
+				console.log('Success: ',this.user);
+			}
+			else {
+				// user not logged in
+				this.user = null;
+				console.log('Fail: ',this.user);
+			}
+		});
+
+		//this.item = af.database.object('/item');
+	}
+
+	login() {
+		this.af.auth.login({
+			provider: AuthProviders.Google
+		});
+	}
+
+	logout() {
+		this.af.auth.logout();
+	}
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AngularFireModule } from 'angularfire2';
+import { 
+  AngularFireModule, 
+  AuthMethods, 
+  AuthProviders 
+} from "angularfire2";
 
 import { AppComponent } from './app.component';
 
 // Must export the config
 export const firebaseConfig = {
-  apiKey: 'AIzaSyAzrtKXwKL5kAoa9dbGt9x32YWA7Yi_Jec',
-  authDomain: 'test-fad8a.firebaseapp.com',
-  databaseURL: 'https://test-fad8a.firebaseio.com',
-  storageBucket: 'test-fad8a.appspot.com',
-  messagingSenderId: "113730117393"
+    apiKey: "AIzaSyDveCJtWCu1zhyvqOBr9my4-n8_eqoeWeQ",
+    authDomain: "oauth-b4994.firebaseapp.com",
+    databaseURL: "https://oauth-b4994.firebaseio.com",
+    storageBucket: "oauth-b4994.appspot.com",
+    messagingSenderId: "803681908280"
 };
 
 @NgModule({
@@ -23,7 +27,10 @@ export const firebaseConfig = {
     BrowserModule,
     FormsModule,
     HttpModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(firebaseConfig,{
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
