88ad215e7017224223261b8258ccd2becf9453ef
daslicht <daslicht@ansolas.de>
2016-10-25 18:03:22 +0200

    re-indent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,33 +2,32 @@ import { Component } from '@angular/core';
 import { FirebaseRestService } from './firebase-rest.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [FirebaseRestService]
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
+	providers: [FirebaseRestService]
 })
 
 export class AppComponent {
 
-  response: string;
+	response: string;
 
-  constructor(private firebaseService: FirebaseRestService, ) {
+	constructor(private firebaseService: FirebaseRestService, ) {
 
-  }
+	}
 
-  setUser() {
-    this.firebaseService.setUser("foo", "bar").subscribe(
-      user => this.response = JSON.stringify(user),
-      error => console.log(error)
-    );
-  }
+	setUser() {
+		this.firebaseService.setUser("foo", "bar").subscribe(
+			user => this.response = JSON.stringify(user),
+			error => console.log(error)
+		);
+	}
 
-  getUser() {
-    this.firebaseService.getUser().subscribe(
-      user => this.response = JSON.stringify(user),
-      error => console.log(error)
-    );
-  }
-
-
+	getUser() {
+		this.firebaseService.getUser().subscribe(
+			user => this.response = JSON.stringify(user),
+			error => console.log(error)
+		);
+	}
+	
 }
