ef0fd1a31515b9809f7e1e1e431edee988a20701
daslicht <daslicht@ansolas.de>
2016-10-26 10:08:34 +0200

    added zone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, NgZone } from '@angular/core';
 import {
 	AngularFire,
 	FirebaseObjectObservable,
@@ -28,7 +28,7 @@ export class AppComponent {
 	};
 	image: string;
 	storageRef;
-	constructor(private af: AngularFire, @Inject(FirebaseApp) firebaseApp: any) {
+	constructor(private af: AngularFire, @Inject(FirebaseApp) firebaseApp: any, zone:NgZone) {
 		this.sizeSubject = new Subject();
 		this.af.auth.subscribe(user => {
 			if (user) {
@@ -42,13 +42,16 @@ export class AppComponent {
 						equalTo: this.sizeSubject
 					}
 				});
+
 				//this.item = af.database.object('/item');
 				this.items.subscribe(queriedItems => {
 					console.log("queriedItems: ",queriedItems);  
 				});
 
 				this.storageRef = firebaseApp.storage().ref().child('images/image.png');
-				this.storageRef.getDownloadURL().then(url => this.image = url);
+				zone.run(() => { 
+					this.storageRef.getDownloadURL().then(url => this.image = url);
+				})
 
 				// var storage = firebaseApp.storage();
 				// console.log(storage);
