87a12dc7fea992f54ff14c48ae168278c4632ca3
daslicht <daslicht@ansolas.de>
2016-11-03 11:47:13 +0100

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,8 +21,6 @@ export class AppComponent {
 	title = 'app works!';
 	user = null;
 	sizeSubject: BehaviorSubject<any>;
-
-	//item: FirebaseObjectObservable<any>;
 	items: FirebaseListObservable<any>;
 	name;
 	_firebaseApp;
@@ -36,36 +34,20 @@ export class AppComponent {
 	storageRef;
 	_zone;
 	constructor(private _af: AngularFire, @Inject(FirebaseApp) _firebaseApp: any, _zone:NgZone) {
-<<<<<<< HEAD
-		this.sizeSubject = new BehaviorSubject('');
-=======
+		this.sizeSubject = new BehaviorSubject('init value');
 		this._firebaseApp = _firebaseApp;
 		this._zone = _zone;
-		this.sizeSubject = new Subject();
->>>>>>> 7404afb6ed8a274c9771384a4bbff2e763d33130
+
 		this._af.auth.subscribe(user => {
 			if (user) {
-				// user logged in
-				this.user = user;
-				//console.log('Success: ',this.user);
 
-		
-				// this.items = this._af.database.list('/items', {
-				// 	query: {
-				// 		orderByChild: 'size',
-				// 		equalTo: this.sizeSubject
-				// 	}
-				// });
+				this.user = user;
+
 				this.items = this._af.database.list('/items', {
 
 				});
 
-				// this.items = _af.database.list('/items', {
-		
-				// });
 
-				//this.item = af.database.object('/item');
-				//this.filterBy(null);
 				this.items.subscribe(queriedItems => {
 					console.log("queriedItems: ",queriedItems);  					
 				});
