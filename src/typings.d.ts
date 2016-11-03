8fa7cdf1734f64562e8d26ffe0a90b6294e8092f
daslicht <daslicht@ansolas.de>
2016-11-03 07:01:21 +0100

    Updated to AngularFire 2.0.0-beta.6

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,5 +2,5 @@
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
 declare var System: any;
-declare var require: any;
-declare var module: any;
+declare var require: NodeRequire;
+declare var module: NodeModule;
