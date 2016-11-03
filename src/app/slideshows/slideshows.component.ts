3ce2cf807e1924725f1a9832ffa8ee4d77c257dd
daslicht <daslicht@ansolas.de>
2016-11-03 17:22:42 +0100

    to much to list

--- src/app/slideshows/slideshows.component.ts
+++ src/app/slideshows/slideshows.component.ts
@@ -13,7 +13,7 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 @Component({
 	selector: 'app-slideshows',
 	templateUrl: './slideshows.component.html',
-	styleUrls: ['./slideshows.component.css']
+	styleUrls: ['./slideshows.component.scss']
 })
 export class SlideshowsComponent implements OnInit {
 
@@ -48,51 +48,116 @@ export class SlideshowsComponent implements OnInit {
 			console.info('Slide Name:' , name);
 			console.info('selected key' , this.selectedSlideshow.$key);
 
-			// ADD SLIDE TO SLIDESHOW inline WORKING!
-			// this._af.database.list('/slideshows/'+this.selectedSlideshow.$key).push({
-			//   name:name
-			// });
-
 			this._af.database.list('/slides/').push({
 				slideshowKey: this.selectedSlideshow.$key,
 				name:name
 			})
-
-			// .then((snapshot)=>{
-			//   	console.log("snapshot: ",snapshot);
-			// });
-				// key.subscribe(queriedItems => {
-				// 	console.log("queriedItems: ",queriedItems);
-				// });
-
-			/*
-					firebase.database().ref('/users'+'/'+'-KV_qMVWLtUCxmNZ7zka').push({
-					ref: userId,
-					username: name,
-					email: email,
-					profile_picture : imageUrl
-				});
-			}*/
-			// let key = this._firebaseApp.ref('/slideshows/' + this.selectedSlideshow.$key + '/' + name);
-			// key.on('value', function(snapshot) {
-			//     console.log('snapshot val:',snapshot.val());
-			// });
-
-			//let slides = this._af.database.list('/slides'+this.selectedSlideshow.$key, {});
-			//let storageRef = this._firebaseApp.ref('/slides'+this.selectedSlideshow.$key);
-			 //   storageRef.push({test:"toast"});
-
-		 // var newPostKey = firebase.database().ref().child('posts').push().key;
-
 	}
 
-			// this.selectedSlideshow.slides.push({
-			//   name: name
-			// });
+	deleteSelectedSlideshow(key:string) {
+		console.log('remove: ', key);
+		this.slideshows.remove(key);
+	}
 
 	deleteAll() {
 		this.slideshows.remove();
 		this.slides.remove();
+	}
+    resize (img, MAX_WIDTH:number = 900, MAX_HEIGHT:number = 900){
+        var canvas = document.createElement("canvas");
+
+        console.log("Size Before: " + img.src.length + " bytes");
+
+        var width = img.width;
+        var height = img.height;
+
+        if (width > height) {
+            if (width > MAX_WIDTH) {
+                height *= MAX_WIDTH / width;
+                width = MAX_WIDTH;
+            }
+        } else {
+            if (height > MAX_HEIGHT) {
+                width *= MAX_HEIGHT / height;
+                height = MAX_HEIGHT;
+            }
+        }
+        canvas.width = width;
+        canvas.height = height;
+        var ctx = canvas.getContext("2d");
+
+        ctx.drawImage(img, 0, 0, width, height);
+
+        var dataUrl = canvas.toDataURL('image/jpeg');  
+        // IMPORTANT: 'jpeg' NOT 'jpg'
+        console.log("Size After:  " + dataUrl.length  + " bytes");
+        return dataUrl
+    }
+	deleteSlide( slide ) {
+		console.log(slide.path)
+		console.log(slide.$key)
+
+	
+		let storageRef = this._firebaseApp.storage().ref().child(slide.path);
+		storageRef.delete().then( () => {
+			console.log('success');
+			this.slides.remove(slide.$key);
+		}).catch( (error)=> {
+			console.log('error');
+		});
+	}
+	
+	addImageToSelectedSlideshow(event, slide_key) {
+		console.log(event, slide_key)
+		let d = new Date();
+		let time = d.getTime();
+		let storageRef = this._firebaseApp.storage().ref().child('images/'+time+'.png');
+		console.log("storageRef",storageRef);
+		var files = event.srcElement.files;
+		var file = files[0]; 
+ 		//let files_src: string[] = [];
+
+        // for (var i = 0; i < files.length; i++) {
+
+        //     files_src.push(files[i]);
+		// 	console.log('files_src',files_src)	
+        //     // Create an img element and add the image file data to it
+        //     var img = document.createElement("img");
+        //     img.src = window.URL.createObjectURL(files[i]);
+
+        //     // Create a FileReader
+        //     var reader = new FileReader();
+
+        //     // Add an event listener to deal with the file when the reader is complete
+        //     reader.addEventListener("load", (event) => {
+        //         // Get the event.target.result from the reader (base64 of the image)
+        //         img.src = event.currentTarget.result;
+
+		// 		console.log('event result',event.currentTarget.result)
+        //         // Resize the image
+        //         var resized_img = this.resize(img);
+		//  		file = resized_img; 
+		// 		 console.log('resized')
+		// 		});
+        //         // Push the img src (base64 string) into our array that we display in our html template
+        //         files_src.push(resized_img);
+        //     }, false);
+
+        //     reader.readAsDataURL(files[i]);
+        // }
+
+			storageRef.put(file).then( (snapshot) => {
+				console.log('Uploaded a blob or file!',snapshot);
+				console.log(snapshot.a.downloadURLs[0]);
+				//this._zone.run( () => { 
+				this.slides.update( slide_key, { 
+					path: storageRef.a.path,
+					url: snapshot.a.downloadURLs[0],
+					name: file.name
+				});
+			});
+			
+
 	}
 
 	constructor( private _af: AngularFire, @Inject(FirebaseApp) _firebaseApp: any, _zone:NgZone) {
